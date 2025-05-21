6c8a8b4c6163899080768c5f04b330f5a75d68e1
Martin Kleiven <m.klei@live.no>
2025-05-21 10:07:15 +0200

    blah blah blah

--- src/lib/components/DiveDetailItem.svelte
+++ src/lib/components/DiveDetailItem.svelte
@@ -5,13 +5,23 @@
   // import { nb } from 'date-fns/locale';
   // And then use it in formatDateFns: formatDateFns(value, dateFormat, { locale: nb })
 
-  export let value: string | Date | undefined | null = undefined; // Allow Date objects
-  export let subValue: string | undefined | null = undefined;
-  export let icon: ComponentType; // Added to fix linter error: 'icon' was not defined
-  export let iconClass: string = 'w-5 h-5 mr-2 text-teal-600';
-  export let textClass: string = 'text-lg'; // Default text class
-  export let showIfValueIsNA: boolean = false; // Controls if the item shows when value is 'N/A'
-  export let dateFormat: string = 'PPP'; // Default date-fns format (e.g., "MMM d, yyyy")
+  let {
+    value = undefined as string | Date | undefined | null,
+    subValue = undefined as string | undefined | null,
+    icon,
+    iconClass = 'w-5 h-5 mr-2 text-teal-600',
+    textClass = 'text-lg',
+    showIfValueIsNA = false,
+    dateFormat = 'PPP'
+  }: {
+    value?: string | Date | undefined | null;
+    subValue?: string | undefined | null;
+    icon: ComponentType;
+    iconClass?: string;
+    textClass?: string;
+    showIfValueIsNA?: boolean;
+    dateFormat?: string;
+  } = $props();
 </script>
 
 {#if value && (value !== 'N/A' || showIfValueIsNA)}
